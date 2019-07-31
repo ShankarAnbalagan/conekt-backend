a26425448113decec78301b67481f7bb9ea2c85f
Shankar Anbalagan <bravoshankar@gmail.com>
2019-07-31 12:39:16 +0530

    Register user functionality added

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+var registerUser = require('./registerUser');
+
+module.exports={registerUser};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
+var {registerUser}=require('./../controllers/index');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* POST Add new user */
+router.post('/register',registerUser);
 
 module.exports = router;
