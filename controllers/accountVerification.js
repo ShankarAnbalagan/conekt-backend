675b21a452371a0e4a0ec39cad6defbb5db02703
Shankar Anbalagan <bravoshankar@gmail.com>
2019-07-31 16:31:36 +0530

    Email verification bug fix

--- controllers/accountVerification.js
+++ controllers/accountVerification.js
@@ -1,22 +1,28 @@
 var userData=require('./../models/UserData');
 
 module.exports= function(req,res,next){
-    userData.updateOne({
-        verificationToken:req.params.code
-    },{
-        set:{
-            isVerified:true
-        },
-        unset:{
-            verificationToken:''
+    console.log(req.query.code);
+    var code=req.query.code;
+
+    userData.findOne({verificationToken:code},function(err,user){
+        if(err) return res.status(200).json({"message":"Something went wrong","data":{}});
+        if(!user) return res.status(422).json({"message":"Link Invalid or Expired","data":{}});
+        else{
+            userData.updateOne({
+                verificationToken:code
+            },{
+                $set:{
+                    isVerified:true
+                },
+                $unset:{
+                    verificationToken:''
+                }
+            },function(err,user){
+                if(err) return res.status(200).json({"message":"Something went wrong","data":{}});
+        
+                res.status(200).json({"message":"Account Verified Successfully","data":""})
+            });
         }
-    },function(err,user){
-        if(err) return res.status(200).json({"message":"Something went wrong","data":{}})
-
-        if(!user) return res.status(422).json({"message":"Link Invalid or Expired","data":{}})
-
-        res.status(200).json({"message":"Account Verified Successfully","data":""})
     });
-
 
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var {registerUser}=require('./../controllers/index');
+var {registerUser,accountVerification}=require('./../controllers/index');
 var {validateRequest,validation_rules}=require('./../utils/index');
 var router = express.Router();
 
 /* POST Add new user */
 router.post('/register',validateRequest(validation_rules.register),registerUser);
+
+router.get('/verify',accountVerification);
 
 module.exports = router;
