1adea5105ae33398d98a207ea9f9a5ced206ffe3
Shivani T Eswara <shivanite@gmail.com>
2019-08-03 13:23:14 +0530

    profileapi changes

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,4 +1,10 @@
 var userData=require('./../models/UserData');
 
 module.exports=function(req,res,next){
+    jwt.verify(req.headers.usertoken,process.env.JWT__SECRET),
+    function(err,data){
+        if(err) console.log(err);
+
+        
+    }
 }
