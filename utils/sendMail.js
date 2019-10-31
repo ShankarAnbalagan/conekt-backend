caa9719b33c3bb282983a0fa8e8fd4fd21cb6b28
Shankar Anbalagan <bravoshankar@gmail.com>
2019-10-31 22:45:30 +0530

    Heroku bug fix

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -5,6 +5,8 @@ module.exports={verification:
             var transporter=nodemailer.createTransport({
                 service: 'gmail',
                 host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
                 auth: {
                     user: process.env.SMTP_USER,
                     pass: process.env.SMTP_PASS
